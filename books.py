b02558af989942bda61136ce529245ebb1813197
Bogdan Hasan <bogdan.hasan@yahoo.com>
2026-05-06 17:27:26 +0300

    Added explicit HTTP status codes

--- books.py
+++ books.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel, Field
 from typing import Optional
+from starlette import status
 
 
 app = FastAPI()
@@ -62,12 +63,12 @@ def find_book_id(book: Book) -> Book:
     return book
 
 
-@app.get('/books')
+@app.get('/books', status_code=status.HTTP_200_OK)
 async def get_all_books():
     return BOOKS
 
 
-@app.get('/book/{book_id}')
+@app.get('/book/{book_id}', status_code=status.HTTP_200_OK)
 async def get_book_by_id(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
@@ -76,26 +77,26 @@ async def get_book_by_id(book_id: int = Path(gt=0)):
     raise HTTPException(status_code=404, detail="The book does not exist")
         
 
-@app.get('/book/')
+@app.get('/book/', status_code=status.HTTP_200_OK)
 async def get_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
     books_to_return = [book for book in BOOKS if book.rating == book_rating]
 
     return books_to_return
 
-@app.get('/books/{published_date}')
+@app.get('/books/{published_date}', status_code=status.HTTP_200_OK)
 async def get_book_by_published_date(published_date: str = Path(min_length=8, max_length=8)):
     books_to_return = [book for book in BOOKS if book.published_date == published_date]
 
     return books_to_return
 
 
-@app.post('/create_book')
+@app.post('/create_book', status_code=status.HTTP_201_CREATED)
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
     BOOKS.append(find_book_id(new_book))
 
 
-@app.put('/books/update_book')
+@app.put('/books/update_book', status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(book_request: BookRequest):
     book_found = False
     for i in range(0, len(BOOKS)):
@@ -109,7 +110,7 @@ async def update_book(book_request: BookRequest):
         raise HTTPException(status_code=404, detail="Book does not exist!")
     
 
-@app.delete('/books/{book_id}')
+@app.delete('/books/{book_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id: int = Path(gt=0)):
     book_found = False
     for i in range(0, len(BOOKS)):
